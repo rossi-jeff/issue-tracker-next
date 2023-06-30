53f4725c52a27fd8f985004e06683599347de00b
Jeff Rossi <jrossi@intouchgps.com>
2023-06-30 10:26:38 -0400

    query string filtering

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,68 +1,73 @@
-"use client";
+'use client'
 
-import useSWR from "swr";
-import { apiUrl } from "../../lib/api-url";
-import { fetcher } from "../../lib/fetcher";
-import { IssueType } from "@/types/issue.type";
-import IssueCard from "./issue-card";
-import { useState } from "react";
-import { UserType } from "@/types/user.type";
-import IssueFilter from "./issue-filter";
-import PaginationControls from "../pagination-controls";
+import useSWR from 'swr'
+import { apiUrl } from '../../lib/api-url'
+import { fetcher } from '../../lib/fetcher'
+import { IssueType } from '@/types/issue.type'
+import IssueCard from './issue-card'
+import { useState } from 'react'
+import { UserType } from '@/types/user.type'
+import IssueFilter from './issue-filter'
+import PaginationControls from '../pagination-controls'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useSwrWithQuery } from '../../lib/use-swr-with-query'
 
 export default function IssuesPage() {
-  const [limit, setLimit] = useState(10);
-  const [offset, setOffset] = useState(0);
-  const [rendered, setRendered] = useState(true);
+	const [limit, setLimit] = useState(10)
+	const [offset, setOffset] = useState(0)
+	const router = useRouter()
+	const search = useSearchParams()
 
-  let issues: IssueType[] = [];
-  let users: UserType[] = [];
+	let issues: IssueType[] = []
+	let users: UserType[] = []
 
-  const issueReq = useSWR(`${apiUrl}/issue`, fetcher);
-  const userReq = useSWR(`${apiUrl}/user`, fetcher);
+	const issueReq = useSwrWithQuery('issue', search.toString())
+	const userReq = useSWR(`${apiUrl}/user`, fetcher)
 
-  if (issueReq.isLoading || userReq.isLoading) return <div>Loading...</div>;
-  if (issueReq.error || userReq.error) return <div>Error</div>;
+	if (issueReq.isLoading || userReq.isLoading) return <div>Loading...</div>
+	if (issueReq.error || userReq.error) return <div>Error</div>
 
-  issues = issueReq.data;
-  users = userReq.data;
+	issues = issueReq.data
+	users = userReq.data
 
-  const pageChanged = (newPage: number) => {
-    setOffset((newPage - 1) * limit);
-  };
+	const pageChanged = (newPage: number) => {
+		setOffset((newPage - 1) * limit)
+	}
 
-  const limitChanged = (newLimit: number) => {
-    setLimit(newLimit);
-    setOffset(0);
-  };
+	const limitChanged = (newLimit: number) => {
+		setLimit(newLimit)
+		setOffset(0)
+	}
 
-  const filterIssues = async (url: string) => {
-    const result = await fetch(url);
-    if (result.ok) {
-      setRendered(false);
-      issues = await result.json();
-      setOffset(0);
-      setRendered(true);
-    }
-  };
+	const filterIssues = async (params: { [key: string]: string }) => {
+		const search = new URLSearchParams()
+		for (const key in params) {
+			search.append(key, params[key])
+		}
+		const queryStr = search.toString()
+		if (queryStr.length) {
+			router.push(`/issues?${queryStr}`)
+		} else {
+			router.push('/issues')
+		}
+	}
 
-  return (
-    <div>
-      <h1>Issues</h1>
-      <IssueFilter users={users} filterIssues={filterIssues} />
-      {rendered &&
-        issues
-          .slice(offset, offset + limit)
-          .map((issue) => <IssueCard key={issue.Id} issue={issue} />)}
-      {rendered && (
-        <PaginationControls
-          limit={limit}
-          offset={offset}
-          count={issues.length}
-          pageChanged={pageChanged}
-          limitChanged={limitChanged}
-        />
-      )}
-    </div>
-  );
+	return (
+		<div>
+			<h1>Issues</h1>
+			<IssueFilter users={users} filterIssues={filterIssues} />
+			{issues &&
+				issues.length &&
+				issues
+					.slice(offset, offset + limit)
+					.map((issue) => <IssueCard key={issue.Id} issue={issue} />)}
+			<PaginationControls
+				limit={limit}
+				offset={offset}
+				count={issues.length}
+				pageChanged={pageChanged}
+				limitChanged={limitChanged}
+			/>
+		</div>
+	)
 }
--- app/time_clocks/time-clock-filter.tsx
+++ app/time_clocks/time-clock-filter.tsx
@@ -1,75 +1,182 @@
-import { getFullName } from "@/lib/get-full-name";
-import { IssueType } from "@/types/issue.type";
-import { ProjectType } from "@/types/project.type";
-import { UserType } from "@/types/user.type";
+import { getFullName } from '@/lib/get-full-name'
+import { IssueType } from '@/types/issue.type'
+import { ProjectType } from '@/types/project.type'
+import { UserType } from '@/types/user.type'
+import { useState } from 'react'
 
 export default function TimeClockFilter({
-  users,
-  projects,
-  issues,
+	users,
+	projects,
+	issues,
+	filterTimeClocks,
 }: {
-  users: UserType[];
-  projects: ProjectType[];
-  issues: IssueType[];
+	users: UserType[]
+	projects: ProjectType[]
+	issues: IssueType[]
+	filterTimeClocks: Function
 }) {
-  const toggle = () => {
-    const el = document.getElementById("time-clock-filter-content");
-    if (el) el.classList.toggle("open");
-  };
-  return (
-    <div className="card" id="time-clock-filter">
-      <button onClick={toggle}>Filter</button>
-      <div className="filter-content" id="time-clock-filter-content">
-        <div className="flex flex-wrap justify-between">
-          <div>
-            <label htmlFor="User">User</label>
-            <select name="User">
-              <option value="">- select -</option>
-              {users.map((u) => (
-                <option key={u.UUID} value={u.Id}>
-                  {getFullName(u)}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="Project">Project</label>
-            <select name="Project">
-              <option value="">- select -</option>
-              {projects.map((p) => (
-                <option key={p.UUID} value={p.Id}>
-                  {p.Sequence ? p.Sequence.Prefix : "N/A"} | {p.Name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="Issue">Issue</label>
-            <select name="Issue">
-              <option value="">- select -</option>
-              {issues.map((i) => (
-                <option key={i.UUID} value={i.Id}>
-                  {i.SequenceNumber} | {i.Title}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-        <div className="flex flex-wrap justify-between">
-          <div>
-            <label htmlFor="StartDate">Start Date</label>
-            <input type="date" name="StartDate" />
-          </div>
-          <div>
-            <label htmlFor="EndDate">End Date</label>
-            <input type="date" name="EndDate" />
-          </div>
-          <div>
-            <label>&nbsp;</label>
-            <button>Clear Filters</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const params = new URLSearchParams(location.search)
+	const [filter, setFilter] = useState<{ [key: string]: string }>({
+		UserId: params.get('UserId') || '',
+		ProjectId: params.get('ProjectId') || '',
+		IssueId: params.get('IssueId') || '',
+		StartDate: params.get('StartDate') || '',
+		EndDate: params.get('EndDate') || '',
+	})
+
+	const toggle = () => {
+		const el = document.getElementById('time-clock-filter-content')
+		if (el) el.classList.toggle('open')
+	}
+
+	const selectChanged = (ev: any) => {
+		const { name, selectedIndex } = ev.target
+		let value
+		switch (name) {
+			case 'UserId':
+				value = selectedIndex > 0 ? users[selectedIndex - 1].Id : ''
+				break
+			case 'ProjectId':
+				value = selectedIndex > 0 ? projects[selectedIndex - 1].Id : ''
+				break
+			case 'IssueId':
+				value = selectedIndex > 0 ? issues[selectedIndex - 1].Id : ''
+				break
+		}
+		const update = {
+			...filter,
+			[name]: value,
+		}
+		setFilter(update)
+		sendTimeClockFilters(update)
+	}
+
+	const dateChanged = (ev: any) => {
+		const { name, value } = ev.target
+		const update = {
+			...filter,
+			[name]: value,
+		}
+		setFilter(update)
+		sendTimeClockFilters(update)
+	}
+
+	const clearFilters = () => {
+		setFilter({
+			UserId: '',
+			ProjectId: '',
+			IssueId: '',
+			StartDate: '',
+			EndDate: '',
+		})
+		sendTimeClockFilters({})
+		// defaultValue does not set selected index
+		const selects = document.getElementsByClassName('filter-select')
+		for (let i = 0; i < selects.length; i++) {
+			const select = document.getElementById(selects[i].id) as HTMLSelectElement
+			if (select) select.selectedIndex = 0
+		}
+		const dates = document.getElementsByClassName('filter-date')
+		for (let i = 0; i < dates.length; i++) {
+			const date = document.getElementById(dates[i].id) as HTMLInputElement
+			if (date) date.value = ''
+		}
+	}
+
+	const sendTimeClockFilters = (update: { [key: string]: string }) => {
+		const sanitized: { [key: string]: string } = {}
+		for (const key in update) {
+			if (update[key] != '') sanitized[key] = update[key]
+		}
+		filterTimeClocks(sanitized)
+	}
+
+	return (
+		<div className="card" id="time-clock-filter">
+			<button onClick={toggle}>Filter</button>
+			<div className="filter-content" id="time-clock-filter-content">
+				<div className="flex flex-wrap justify-between">
+					<div>
+						<label htmlFor="UserId">User</label>
+						<select
+							name="UserId"
+							id="UserId"
+							className="filter-select"
+							onChange={selectChanged}
+							defaultValue={filter.UserId || ''}
+						>
+							<option value="">- select -</option>
+							{users.map((u) => (
+								<option key={u.UUID} value={String(u.Id)}>
+									{getFullName(u)}
+								</option>
+							))}
+						</select>
+					</div>
+					<div>
+						<label htmlFor="ProjectId">Project</label>
+						<select
+							name="ProjectId"
+							id="ProjectId"
+							className="filter-select"
+							onChange={selectChanged}
+							defaultValue={filter.ProjectId || ''}
+						>
+							<option value="">- select -</option>
+							{projects.map((p) => (
+								<option key={p.UUID} value={String(p.Id)}>
+									{p.Sequence ? p.Sequence.Prefix : 'N/A'} | {p.Name}
+								</option>
+							))}
+						</select>
+					</div>
+					<div>
+						<label htmlFor="IssueId">Issue</label>
+						<select
+							name="IssueId"
+							id="IssueId"
+							className="filter-select"
+							defaultValue={filter.IssueId || ''}
+							onChange={selectChanged}
+						>
+							<option value="">- select -</option>
+							{issues.map((i) => (
+								<option key={i.UUID} value={String(i.Id)}>
+									{i.SequenceNumber} | {i.Title}
+								</option>
+							))}
+						</select>
+					</div>
+				</div>
+				<div className="flex flex-wrap justify-between">
+					<div>
+						<label htmlFor="StartDate">Start Date</label>
+						<input
+							type="date"
+							name="StartDate"
+							id="StartDate"
+							className="filter-date"
+							defaultValue={filter.StartDate || ''}
+							onChange={dateChanged}
+						/>
+					</div>
+					<div>
+						<label htmlFor="EndDate">End Date</label>
+						<input
+							type="date"
+							name="EndDate"
+							id="EndDate"
+							className="filter-date"
+							defaultValue={filter.EndDate || ''}
+							onChange={dateChanged}
+						/>
+					</div>
+					<div>
+						<label>&nbsp;</label>
+						<button onClick={clearFilters}>Clear Filters</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }

14809d6b040be7d260ce57480dcf191ae810c330
Jeff Rossi <jrossi@intouchgps.com>
2023-07-02 09:57:45 -0400

    user edit tinkering

--- app/users/[uuid]/page.tsx
+++ app/users/[uuid]/page.tsx
@@ -1,165 +1,165 @@
-"use client";
+'use client'
 
-import { apiUrl } from "@/lib/api-url";
-import { fetcher } from "@/lib/fetcher";
-import { UserRoleArray } from "@/types/array.types";
-import { UserType } from "@/types/user.type";
-import { useParams } from "next/navigation";
-import useSWR from "swr";
-import PhoneList from "../phone-list";
-import EmailList from "../email-list";
+import { apiUrl } from '@/lib/api-url'
+import { fetcher } from '@/lib/fetcher'
+import { UserRoleArray } from '@/types/array.types'
+import { UserType } from '@/types/user.type'
+import { useParams } from 'next/navigation'
+import useSWR from 'swr'
+import PhoneList from '../phone-list'
+import EmailList from '../email-list'
 
 export default function EditUserPage() {
-  const { uuid } = useParams();
-  let user: UserType = {
-    Credentials: {
-      Username: "",
-    },
-    Name: {
-      First: "",
-      Middle: "",
-      Last: "",
-    },
-    Roles: [],
-    Phones: [],
-    Emails: [],
-  };
-  const userReq = useSWR(uuid ? `${apiUrl}/user/${uuid}` : null, fetcher);
-  if (userReq.isLoading) return <div>Loading...</div>;
-  if (userReq.error) return <div>Error</div>;
+	const { uuid } = useParams()
+	let user: UserType = {
+		Credentials: {
+			Username: '',
+		},
+		Name: {
+			First: '',
+			Middle: '',
+			Last: '',
+		},
+		Roles: [],
+		Phones: [],
+		Emails: [],
+	}
+	const userReq = useSWR(uuid ? `${apiUrl}/user/${uuid}` : null, fetcher)
+	if (userReq.isLoading) return <div>Loading...</div>
+	if (userReq.error) return <div>Error</div>
 
-  user = userReq.data;
+	user = userReq.data
 
-  const createEmail = (ev: {
-    Address: string;
-    Usage: string;
-    Public: boolean;
-  }) => {
-    console.log(ev);
-  };
+	const createEmail = (ev: {
+		Address: string
+		Usage: string
+		Public: boolean
+	}) => {
+		console.log(ev)
+	}
 
-  const updateEmail = (ev: {
-    Address: string;
-    Usage: string;
-    Public: boolean;
-    UUID: string;
-  }) => {
-    console.log(ev);
-  };
+	const updateEmail = (ev: {
+		Address: string
+		Usage: string
+		Public: boolean
+		UUID: string
+	}) => {
+		console.log(ev)
+	}
 
-  const deleteEmail = (uuid: string) => {
-    console.log(uuid);
-  };
+	const deleteEmail = (uuid: string) => {
+		console.log(uuid)
+	}
 
-  const createPhone = (ev: {
-    Number: string;
-    Type: string;
-    Usage: string;
-    Public: boolean;
-  }) => {
-    console.log(ev);
-  };
+	const createPhone = (ev: {
+		Number: string
+		Type: string
+		Usage: string
+		Public: boolean
+	}) => {
+		console.log(ev)
+	}
 
-  const updatePhone = (ev: {
-    Number: string;
-    Type: string;
-    Usage: string;
-    Public: boolean;
-    UUID: string;
-  }) => {
-    console.log(ev);
-  };
+	const updatePhone = (ev: {
+		Number: string
+		Type: string
+		Usage: string
+		Public: boolean
+		UUID: string
+	}) => {
+		console.log(ev)
+	}
 
-  const deletePhone = (uuid: string) => {
-    console.log(uuid);
-  };
-  return (
-    <div className="card">
-      <h2>Edit User</h2>
-      <div className="flex flex-wrap justify-between">
-        <div>
-          <label htmlFor="Username" className="block">
-            Username
-          </label>
-          {user.Credentials && (
-            <input
-              type="text"
-              name="Username"
-              id="Username"
-              defaultValue={user.Credentials.Username}
-            />
-          )}
-        </div>
-      </div>
-      <div className="flex flex-wrap justify-between">
-        <div>
-          <label htmlFor="First" className="block">
-            First
-          </label>
-          {user.Name && (
-            <input
-              type="text"
-              name="First"
-              id="First"
-              defaultValue={user.Name.First}
-            />
-          )}
-        </div>
-        <div>
-          <label htmlFor="Middle" className="block">
-            Middle
-          </label>
-          {user.Name && (
-            <input
-              type="text"
-              name="Middle"
-              id="Middle"
-              defaultValue={user.Name.Middle}
-            />
-          )}
-        </div>
-        <div>
-          <label htmlFor="Last" className="block">
-            Last
-          </label>
-          {user.Name && (
-            <input
-              type="text"
-              name="Last"
-              id="Last"
-              defaultValue={user.Name.Last}
-            />
-          )}
-        </div>
-      </div>
-      <div className="flex flex-wrap justify-between">
-        {UserRoleArray.map((role, idx) => (
-          <div key={idx}>
-            <input
-              type="checkbox"
-              name="role"
-              id={"role-" + role}
-              value={role}
-              checked={user.Roles && user.Roles.indexOf(role) > -1}
-            />
-            <label htmlFor={"role-" + role} className="ml-2">
-              {role}
-            </label>
-          </div>
-        ))}
-      </div>
-      <PhoneList
-        phones={user.Phones || []}
-        createPhone={createPhone}
-        updatePhone={updatePhone}
-        deletePhone={deletePhone}
-      />
-      <EmailList
-        emails={user.Emails || []}
-        createEmail={createEmail}
-        updateEmail={updateEmail}
-        deleteEmail={deleteEmail}
-      />
-    </div>
-  );
+	const deletePhone = (uuid: string) => {
+		console.log(uuid)
+	}
+	return (
+		<div className="card">
+			<h2>Edit User</h2>
+			<div className="flex flex-wrap justify-between">
+				<div>
+					<label htmlFor="Username" className="block">
+						Username
+					</label>
+					{user.Credentials && (
+						<input
+							type="text"
+							name="Username"
+							id="Username"
+							defaultValue={user.Credentials.Username}
+						/>
+					)}
+				</div>
+			</div>
+			<div className="flex flex-wrap justify-between">
+				<div>
+					<label htmlFor="First" className="block">
+						First
+					</label>
+					{user.Name && (
+						<input
+							type="text"
+							name="First"
+							id="First"
+							defaultValue={user.Name.First}
+						/>
+					)}
+				</div>
+				<div>
+					<label htmlFor="Middle" className="block">
+						Middle
+					</label>
+					{user.Name && (
+						<input
+							type="text"
+							name="Middle"
+							id="Middle"
+							defaultValue={user.Name.Middle}
+						/>
+					)}
+				</div>
+				<div>
+					<label htmlFor="Last" className="block">
+						Last
+					</label>
+					{user.Name && (
+						<input
+							type="text"
+							name="Last"
+							id="Last"
+							defaultValue={user.Name.Last}
+						/>
+					)}
+				</div>
+			</div>
+			<div className="flex flex-wrap justify-between">
+				{UserRoleArray.map((role, idx) => (
+					<div key={idx}>
+						<input
+							type="checkbox"
+							name="role"
+							id={'role-' + role}
+							value={role}
+							defaultChecked={user.Roles && user.Roles.indexOf(role) > -1}
+						/>
+						<label htmlFor={'role-' + role} className="ml-2">
+							{role}
+						</label>
+					</div>
+				))}
+			</div>
+			<PhoneList
+				phones={user.Phones || []}
+				createPhone={createPhone}
+				updatePhone={updatePhone}
+				deletePhone={deletePhone}
+			/>
+			<EmailList
+				emails={user.Emails || []}
+				createEmail={createEmail}
+				updateEmail={updateEmail}
+				deleteEmail={deleteEmail}
+			/>
+		</div>
+	)
 }
--- app/users/email-list.tsx
+++ app/users/email-list.tsx
@@ -1,248 +1,254 @@
-"use client";
+'use client'
 
-import { UsageArray } from "@/types/array.types";
-import { EmailType } from "@/types/email.type";
-import { useState } from "react";
+import { UsageArray } from '@/types/array.types'
+import { EmailType } from '@/types/email.type'
+import { useState } from 'react'
 
 export default function EmailList({
-  emails,
-  createEmail,
-  updateEmail,
-  deleteEmail,
+	emails,
+	createEmail,
+	updateEmail,
+	deleteEmail,
 }: {
-  emails: EmailType[];
-  createEmail: Function;
-  updateEmail: Function;
-  deleteEmail: Function;
+	emails: EmailType[]
+	createEmail: Function
+	updateEmail: Function
+	deleteEmail: Function
 }) {
-  const [emailNew, setEmailNew] = useState<EmailType>({
-    Address: "",
-    Usage: "Personal",
-    Public: false,
-  });
-  const [emailEdit, setEmailEdit] = useState<EmailType>({});
+	const [emailNew, setEmailNew] = useState<EmailType>({
+		Address: '',
+		Usage: 'Personal',
+		Public: false,
+	})
+	const [emailEdit, setEmailEdit] = useState<EmailType>({
+		Address: '',
+		Usage: 'Personal',
+		Public: false,
+	})
 
-  const newFieldChanged = (ev: any) => {
-    const { name, value, checked } = ev.target;
-    let update: EmailType = {};
-    switch (name) {
-      case "Address":
-      case "Usage":
-        update = {
-          ...emailEdit,
-          [name]: value,
-        };
-        break;
-      case "Public":
-        update = {
-          ...emailEdit,
-          [name]: checked,
-        };
-        break;
-    }
-    setEmailNew(update);
-  };
+	const newFieldChanged = (ev: any) => {
+		const { name, value, checked } = ev.target
+		let update: EmailType = {}
+		switch (name) {
+			case 'Address':
+			case 'Usage':
+				update = {
+					...emailNew,
+					[name]: value,
+				}
+				break
+			case 'Public':
+				update = {
+					...emailNew,
+					[name]: checked,
+				}
+				break
+		}
+		console.log('newFieldChanged', JSON.stringify(update))
+		setEmailNew(update)
+	}
 
-  const editFieldChanged = (ev: any) => {
-    const { name, value, checked } = ev.target;
-    let update: EmailType = {};
-    switch (name) {
-      case "Address":
-      case "Usage":
-        update = {
-          ...emailEdit,
-          [name]: value,
-        };
-        break;
-      case "Public":
-        update = {
-          ...emailEdit,
-          [name]: checked,
-        };
-        break;
-    }
-    setEmailEdit(update);
-  };
+	const editFieldChanged = (ev: any) => {
+		const { name, value, checked } = ev.target
+		let update: EmailType = {}
+		switch (name) {
+			case 'Address':
+			case 'Usage':
+				update = {
+					...emailEdit,
+					[name]: value,
+				}
+				break
+			case 'Public':
+				update = {
+					...emailEdit,
+					[name]: checked,
+				}
+				break
+		}
+		console.log('editFieldChanged', JSON.stringify(update))
+		setEmailEdit(update)
+	}
 
-  const editEmail = (uuid: string) => {
-    const email = emails.find((e) => e.UUID == uuid) || {
-      Address: "",
-      Usage: "Personal",
-      Public: false,
-    };
-    setEmailEdit(email);
-    showEdit();
-  };
+	const editEmail = (uuid: string) => {
+		const email = emails.find((e) => e.UUID == uuid) || {
+			Address: '',
+			Usage: 'Personal',
+			Public: false,
+		}
+		setEmailEdit(email)
+		showEdit()
+	}
 
-  const createClicked = () => {
-    const { Address, Usage, Public } = emailNew;
-    createEmail({ Address, Usage, Public });
-    setEmailNew({
-      Address: "",
-      Usage: "Personal",
-      Public: false,
-    });
-    hideNew();
-  };
+	const createClicked = () => {
+		const { Address, Usage, Public } = emailNew
+		createEmail({ Address, Usage, Public })
+		setEmailNew({
+			Address: '',
+			Usage: 'Personal',
+			Public: false,
+		})
+		hideNew()
+	}
 
-  const updateClicked = () => {
-    const { Address, Usage, Public, UUID } = emailEdit;
-    updateEmail({ Address, Usage, Public, UUID });
-    hideEdit();
-  };
+	const updateClicked = () => {
+		const { Address, Usage, Public, UUID } = emailEdit
+		updateEmail({ Address, Usage, Public, UUID })
+		hideEdit()
+	}
 
-  const showOverlay = () => {
-    const overlay = document.getElementById("email-overlay");
-    if (overlay) overlay.classList.add("open");
-  };
+	const showOverlay = () => {
+		const overlay = document.getElementById('email-overlay')
+		if (overlay) overlay.classList.add('open')
+	}
 
-  const hideOverlay = () => {
-    const overlay = document.getElementById("email-overlay");
-    if (overlay) overlay.classList.remove("open");
-  };
+	const hideOverlay = () => {
+		const overlay = document.getElementById('email-overlay')
+		if (overlay) overlay.classList.remove('open')
+	}
 
-  const showNew = () => {
-    hideEdit();
-    showOverlay();
-    const dialog = document.getElementById("new-email-dialog");
-    if (dialog) dialog.classList.add("open");
-  };
+	const showNew = () => {
+		hideEdit()
+		showOverlay()
+		const dialog = document.getElementById('new-email-dialog')
+		if (dialog) dialog.classList.add('open')
+	}
 
-  const hideNew = () => {
-    const dialog = document.getElementById("new-email-dialog");
-    if (dialog) dialog.classList.remove("open");
-    hideOverlay();
-  };
+	const hideNew = () => {
+		const dialog = document.getElementById('new-email-dialog')
+		if (dialog) dialog.classList.remove('open')
+		hideOverlay()
+	}
 
-  const showEdit = () => {
-    hideNew();
-    showOverlay();
-    const dialog = document.getElementById("edit-email-dialog");
-    if (dialog) dialog.classList.add("open");
-  };
+	const showEdit = () => {
+		hideNew()
+		showOverlay()
+		const dialog = document.getElementById('edit-email-dialog')
+		if (dialog) dialog.classList.add('open')
+	}
 
-  const hideEdit = () => {
-    const dialog = document.getElementById("edit-email-dialog");
-    if (dialog) dialog.classList.remove("open");
-    hideOverlay();
-  };
-  return (
-    <div>
-      <div className="flex flex-wrap">
-        <h3>Emails</h3>
-        <button onClick={showNew} className="ml-4">
-          New Email
-        </button>
-      </div>
-      {emails.map((email) => (
-        <div key={email.Id} className="flex flex-wrap justify-between">
-          <div>
-            <button onClick={() => editEmail(email.UUID || "")}>Edit</button>
-          </div>
-          <div>{email.Address}</div>
-          <div>{email.Usage}</div>
-          <div>{email.Public ? "Yes" : "No"}</div>
-          <div>
-            <button onClick={() => deleteEmail(email.UUID)}>Delete</button>
-          </div>
-        </div>
-      ))}
-      <div className="modal-overlay" id="email-overlay">
-        <div className="modal-fit" id="new-email-dialog">
-          <h3>New Email</h3>
-          <div className="my-2">
-            <div>
-              <label htmlFor="Address" className="block">
-                Address
-              </label>
-              <input
-                type="email"
-                name="Address"
-                defaultValue={emailNew.Address}
-                onChange={newFieldChanged}
-              />
-            </div>
-            <div>
-              <label htmlFor="Usage" className="block">
-                Usage
-              </label>
-              <select
-                name="Usage"
-                defaultValue={emailNew.Usage}
-                onChange={newFieldChanged}
-              >
-                {UsageArray.map((u, i) => (
-                  <option key={i} value={u}>
-                    {u}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <input
-                type="checkbox"
-                name="Public"
-                defaultChecked={emailNew.Public}
-                onChange={newFieldChanged}
-              />
-              <label htmlFor="Public" className="ml-2">
-                Public
-              </label>
-            </div>
-          </div>
-          <div className="flex flex-wrap justify-between">
-            <button onClick={hideNew}>Cancel</button>
-            <button onClick={createClicked}>Create Email</button>
-          </div>
-        </div>
-        <div className="modal-fit" id="edit-email-dialog">
-          <h3>Edit Email</h3>
-          <div className="my-2">
-            <div>
-              <label htmlFor="Address" className="block">
-                Address
-              </label>
-              <input
-                type="email"
-                name="Address"
-                defaultValue={emailEdit.Address}
-                onChange={editFieldChanged}
-              />
-            </div>
-            <div>
-              <label htmlFor="Usage" className="block">
-                Usage
-              </label>
-              <select
-                name="Usage"
-                defaultValue={emailEdit.Usage}
-                onChange={editFieldChanged}
-              >
-                {UsageArray.map((u, i) => (
-                  <option key={i} value={u}>
-                    {u}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <input
-                type="checkbox"
-                name="Public"
-                defaultChecked={emailEdit.Public}
-                onChange={editFieldChanged}
-              />
-              <label htmlFor="Public" className="ml-2">
-                Public
-              </label>
-            </div>
-          </div>
-          <div className="flex flex-wrap justify-between">
-            <button onClick={hideEdit}>Cancel</button>
-            <button onClick={updateClicked}>Update Email</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const hideEdit = () => {
+		const dialog = document.getElementById('edit-email-dialog')
+		if (dialog) dialog.classList.remove('open')
+		hideOverlay()
+	}
+	return (
+		<div>
+			<div className="flex flex-wrap">
+				<h3>Emails</h3>
+				<button onClick={showNew} className="ml-4">
+					New Email
+				</button>
+			</div>
+			{emails.map((email, i) => (
+				<div key={email.Id || i} className="flex flex-wrap justify-between">
+					<div>
+						<button onClick={() => editEmail(email.UUID || '')}>Edit</button>
+					</div>
+					<div>{email.Address}</div>
+					<div>{email.Usage}</div>
+					<div>{email.Public ? 'Yes' : 'No'}</div>
+					<div>
+						<button onClick={() => deleteEmail(email.UUID)}>Delete</button>
+					</div>
+				</div>
+			))}
+			<div className="modal-overlay" id="email-overlay">
+				<div className="modal-fit" id="new-email-dialog">
+					<h3>New Email</h3>
+					<div className="my-2">
+						<div>
+							<label htmlFor="Address" className="block">
+								Address
+							</label>
+							<input
+								type="email"
+								name="Address"
+								defaultValue={emailNew.Address}
+								onChange={newFieldChanged}
+							/>
+						</div>
+						<div>
+							<label htmlFor="Usage" className="block">
+								Usage
+							</label>
+							<select
+								name="Usage"
+								defaultValue={emailNew.Usage}
+								onChange={newFieldChanged}
+							>
+								{UsageArray.map((u, i) => (
+									<option key={i} value={u}>
+										{u}
+									</option>
+								))}
+							</select>
+						</div>
+						<div>
+							<input
+								type="checkbox"
+								name="Public"
+								defaultChecked={emailNew.Public}
+								onChange={newFieldChanged}
+							/>
+							<label htmlFor="Public" className="ml-2">
+								Public
+							</label>
+						</div>
+					</div>
+					<div className="flex flex-wrap justify-between">
+						<button onClick={hideNew}>Cancel</button>
+						<button onClick={createClicked}>Create Email</button>
+					</div>
+				</div>
+				<div className="modal-fit" id="edit-email-dialog">
+					<h3>Edit Email</h3>
+					<div className="my-2">
+						<div>
+							<label htmlFor="Address" className="block">
+								Address
+							</label>
+							<input
+								type="email"
+								name="Address"
+								defaultValue={emailEdit.Address}
+								onChange={editFieldChanged}
+							/>
+						</div>
+						<div>
+							<label htmlFor="Usage" className="block">
+								Usage
+							</label>
+							<select
+								name="Usage"
+								defaultValue={emailEdit.Usage}
+								onChange={editFieldChanged}
+							>
+								{UsageArray.map((u, i) => (
+									<option key={i} value={u}>
+										{u}
+									</option>
+								))}
+							</select>
+						</div>
+						<div>
+							<input
+								type="checkbox"
+								name="Public"
+								defaultChecked={emailEdit.Public}
+								onChange={editFieldChanged}
+							/>
+							<label htmlFor="Public" className="ml-2">
+								Public
+							</label>
+						</div>
+					</div>
+					<div className="flex flex-wrap justify-between">
+						<button onClick={hideEdit}>Cancel</button>
+						<button onClick={updateClicked}>Update Email</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
